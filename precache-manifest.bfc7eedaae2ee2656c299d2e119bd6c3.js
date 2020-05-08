647996959e76ff4662278e00e88d2493c74e2a7e
vardhman <vardhmanhundia_mahesh@srmuniv.edu.in>
2020-05-08 20:23:04 +0530

    Updates

---
+++ precache-manifest.bfc7eedaae2ee2656c299d2e119bd6c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484264420da927213b21977915f9116b",
+    "url": "/profileWebsite/index.html"
+  },
+  {
+    "revision": "0e1eed0904ce3f917c5d",
+    "url": "/profileWebsite/static/css/main.c38b9f5c.chunk.css"
+  },
+  {
+    "revision": "d9dbe7f2b178d173b691",
+    "url": "/profileWebsite/static/js/2.556d3cf3.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/profileWebsite/static/js/2.556d3cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1eed0904ce3f917c5d",
+    "url": "/profileWebsite/static/js/main.50e9cccc.chunk.js"
+  },
+  {
+    "revision": "0c3c3a5ea204fcf45862",
+    "url": "/profileWebsite/static/js/runtime-main.42c67683.js"
+  },
+  {
+    "revision": "df542c0bd8f855d7f5fa25896fc10ef7",
+    "url": "/profileWebsite/static/media/vardhman.df542c0b.jpg"
+  }
+]);
